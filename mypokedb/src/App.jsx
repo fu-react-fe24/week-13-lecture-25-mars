694c2c2d0f0eb8afb1bd340d0d103ebbbdded09d
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2025-03-26 10:02:44 +0100

    committed

--- mypokedb/src/App.jsx
+++ mypokedb/src/App.jsx
@@ -1,8 +1,12 @@
+import Header from "./components/Header"
+import Pokedex from "./components/Pokedex"
+
 function App() {
 
   return (
     <section className="app">
-      MyPokeDB
+      <Header />
+      <Pokedex />
     </section>
   )
 }
